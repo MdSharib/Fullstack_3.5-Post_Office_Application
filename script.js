8d5bf4c31fee5e79f5199a634e0c906d7c102237
MdSharib <mdsharib15041998@gmail.com>
2023-08-05 12:37:30 +0530

    added home page script

--- script.js
+++ script.js
@@ -1,51 +1,81 @@
 
+const getBtn = document.getElementById("get-btn");
+const ipScreen = document.getElementById("right-ip");
+const TOKEN = '181535a724bccd';
+let userIp = '';
 
-let userData = {};
-const userIp = '';
 
-async function getUserIP() {
-    const URL = 'https://api.ipify.org?format=json';
+
+
+
+
+// async function getUserIP() {
+//     const URL = 'https://api.ipify.org?format=json';
 
     
-    // fetch(apiURL)
-    //     .then(response => response.json())
-    //     .then(data => {
+//     // fetch(apiURL)
+//     //     .then(response => response.json())
+//     //     .then(data => {
             
-    //         console.log(data);
-    //     })
-    //     .catch(error => {
-    //         console.error('Error fetching IP address:', error);
-    //     });
+//     //         console.log(data);
+//     //     })
+//     //     .catch(error => {
+//     //         console.error('Error fetching IP address:', error);
+//     //     });
 
-    const res = await fetch(URL);
-    const data = await res.json();
-    // console.log(data);
-    // userData = data;
-    getUserDetails(data.ip);
-}
-
-
-const getUserDetails = async(currIp) => {
-    const res = await fetch(`https://ipinfo.io/${currIp}/geo`);
-    const data = await res.json();
-    userData = JSON.parse(JSON.stringify(data));
-    extractFromTimeZone();
-    showPostOffices();
-}
+//     const res = await fetch(URL);
+//     const data = await res.json();
+//     // console.log(data);
+//     // userData = data;
+//     getUserDetails(data.ip);
+// }
 
 
-// get time using timezone from user data
-const extractFromTimeZone = () => {
+// const getUserDetails = async(currIp) => {
+//     const res = await fetch(`https://ipinfo.io/json`);
+//     const data = await res.json();
+//     userData = JSON.parse(JSON.stringify(data));
+//     extractFromTimeZone();
+//     showPostOffices();
+// }
+
+
+// // get time using timezone from user data
+// const extractFromTimeZone = () => {
     
-let chicago_datetime_str = new Date().toLocaleString("en-US", { timeZone: userData["timezone"] });
-console.log(chicago_datetime_str);
+// let chicago_datetime_str = new Date().toLocaleString("en-US", { timeZone: userData["timezone"] });
+// console.log(chicago_datetime_str);
+// }
+
+// // display post offices nearby
+// const showPostOffices = async() => {
+//     const res = await fetch(`https://api.postalpincode.in/pincode/${userData['postal']}`);
+//     const data = await res.json();
+//     console.log(data);
+// }
+
+// getUserIP();
+
+const renderIp = () => {
+    ipScreen.innerHTML = `Your Current IP Address is ${userIp}`;
 }
 
-// display post offices nearby
-const showPostOffices = async() => {
-    const res = await fetch(`https://api.postalpincode.in/pincode/${userData['postal']}`);
+
+
+const getIpHandler = async() => {
+    const res = await fetch(`https://ipinfo.io/json?token=${TOKEN}`);
     const data = await res.json();
-    console.log(data);
+    userIp = data.ip; 
+    renderIp(); 
+    // console.log(data);
 }
 
-getUserIP();
+document.addEventListener("DOMContentLoaded", getIpHandler);
+
+const getBtnHandler = () => {
+    window.location.href = "./detailsPage"
+}
+
+
+// /event listeners
+getBtn.addEventListener('click', getBtnHandler);
